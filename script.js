21aff87867367bb26edd940b646afae3aae5de15
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2026-01-03 22:43:33 +0000

    update

--- script.js
+++ script.js
@@ -133,11 +133,12 @@ DH[5][20] = "8pm - 9pm<br>DJ Mix N Match";
 DH[5][22] = "10pm - 12am<br>DJ Songbird";
 
 // SATURDAY (6)
+DH[6][10]  = "10am – 12pm<br>DJ Queen Dani";
 DH[6][0]  = "12am – 2am<br>DJ Songbird";
 DH[6][2]  = "2am – 4am<br>Amar - AJ";
 DH[6][4]  = "4am – 6am<br>Auto";
 DH[6][6]  = "6am – 10am<br>Free";
-DH[6][16] = "4pm – 6pm<br>Free";
+DH[6][16] = "4pm – 6pm<br>DJ Keyes";
 DH[6][18] = "6pm – 7pm<br>Laura - DJ LilDevil";
 DH[6][19] = "7pm – 9pm<br>DJ Bes Wolf";
 DH[6][21] = "9pm – 10pm<br>KSM";
@@ -148,7 +149,7 @@ DH[7][8]  = "8am - 10am<br>DJ Queen Dani";
 DH[7][11] = "11am – 12pm<br>HotShot DJ";
 DH[7][12] = "12pm – 1pm<br>Auto";
 DH[7][13] = "1pm – 3pm<br>JK";           
-DH[7][17] = "5pm – 7pm<br>DJ Keyes";
+DH[7][17] = "5pm – 7pm<br>Free/Auto";
 DH[7][20] = "8pm - 9pm<br>DJ EchoFalls";
 DH[7][21] = "10pm – 12am<br>Leslie";
 
