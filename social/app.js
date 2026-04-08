30c706c2e15de2eb36c4d75892c41083325f9d8a
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2026-04-08 21:33:15 +0100

    Update app.js

--- social/app.js
+++ social/app.js
@@ -1,4 +1,4 @@
-import { firebaseConfig } from './firebase-config.js';
+import { firebaseConfig } from '/social/firebase-config.js';
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
 import {
   getAuth,
