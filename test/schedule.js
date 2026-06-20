e6e8895a4598bf269cc2085fb180dd0d2bd30fa8
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2026-06-20 15:02:39 +0100

    Create schedule.js

---
+++ test/schedule.js
@@ -0,0 +1,20 @@
+const schedule = [
+{
+  dj: "Chanel",
+  start: "18:00",
+  end: "29:00",
+  image: "/images/chanel.jpg"
+},
+{
+  dj: "stephan",
+  start: "20:00",
+  end: "22:00",
+  image: "/images/djdon.jpg"
+},
+{
+  dj: "Free",
+  start: "22:00",
+  end: "00:00",
+  image: "/images/djnala.jpg"
+}
+];
