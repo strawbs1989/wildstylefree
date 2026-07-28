80c8522c18e3d25ff8b08aeeaae032c35a11c4f6
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2026-07-28 16:54:21 +0100

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,3 +1,6 @@
+console.log("NEW SCHEDULE.JS LOADED");
+
+
 const SCHEDULE_URL = "https://script.google.com/macros/s/AKfycby2xfvFxbHKAizMqHrl-p-JqxsGR5D7n7BMKCZhZblDyAm-VHw6VyaXX8vVl7d27Bs/exec";
 
 const DAY_ORDER = [
