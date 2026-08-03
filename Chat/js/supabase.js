a9a6d84b15937552b0d3b5b6500a0f7d74f10930
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2026-08-03 17:33:55 +0100

    Update Supabase key in supabase.js

--- Chat/js/supabase.js
+++ Chat/js/supabase.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL="https://pzdjemvogdpbpinixltc.supabase.co";
-const SUPABASE_KEY="REPLACE_WITH_YOUR_CURRENT_PUBLISHABLE_KEY";
+const SUPABASE_KEY="sb_publishable__DnIH0oiCtOl9tyBCVb9hg_9vwcrxQr";
 const supabase=window.supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
