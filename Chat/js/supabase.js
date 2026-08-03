9408df75105cfa49733599eb9201504c5c375e80
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2026-08-03 19:02:55 +0100

    Add console logging for 'supabase' type and value
    Log the type of 'supabase' and its value to the console.

--- Chat/js/supabase.js
+++ Chat/js/supabase.js
@@ -1,10 +1,2 @@
-const SUPABASE_URL = "https://pzdjemvogdpbpinixltc.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable__DnIH0oiCtOl9tyBCVb9hg_9vwcrxQr";
-
-const { createClient } = supabase;
-
-const client = createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+alert(typeof supabase);
+console.log(supabase);
