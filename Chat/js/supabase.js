389e4ac72dc0c6341518a2155836ca5aa0495c47
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2026-08-06 21:02:41 +0100

    Add files via upload

---
+++ Chat/js/supabase.js
@@ -0,0 +1,12 @@
+alert("SUPABASE.JS LOADED");
+
+const SUPABASE_URL = "https://pzdjemvogdpbpinixltc.supabase.co";
+
+const SUPABASE_KEY="sb_publishable__DnIH0oiCtOl9tyBCVb9hg_9vwcrxQr";
+
+window.client = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+console.log(window.client);
