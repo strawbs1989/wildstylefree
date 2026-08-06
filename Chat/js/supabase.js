61375a0956baf90b445e7b92660fdce142b24b1e
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2026-08-06 19:01:46 +0100

    Update supabase.js

--- Chat/js/supabase.js
+++ Chat/js/supabase.js
@@ -1,11 +1,12 @@
+alert("SUPABASE.JS LOADED");
+
 const SUPABASE_URL = "https://pzdjemvogdpbpinixltc.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable__DnIH0oiCtOl9tyBCVb9hg_9vwcrxQr";
+const SUPABASE_KEY="sb_publishable__DnIH0oiCtOl9tyBCVb9hg_9vwcrxQr";
 
-const client = supabase.createClient(
+window.client = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
 
-console.log("SUPABASE LOADED");
-console.log(client);
+console.log(window.client);
