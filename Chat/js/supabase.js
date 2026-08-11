982417d150dd9d492b1436be9490605d32eadab0
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2026-08-11 16:19:04 +0100

    Update supabase.js

--- Chat/js/supabase.js
+++ Chat/js/supabase.js
@@ -1,10 +1,23 @@
-const SUPABASE_URL = "https://pzdjemvogdpbpinixltc.supabase.co";
+const SUPABASE_URL =
+    "https://pzdjemvogdpbpinixltc.supabase.co";
 
-const SUPABASE_KEY="sb_publishable__DnIH0oiCtOl9tyBCVb9hg_9vwcrxQr";
+const SUPABASE_KEY =
+    "sb_publishable__DnIH0oiCtOl9tyBCVb9hg_9vwcrxQr";
+
 
 window.client = supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_KEY
+    SUPABASE_KEY,
+    {
+        auth: {
+            persistSession: true,
+            autoRefreshToken: true,
+            detectSessionInUrl: false,
+            storage: window.localStorage
+        }
+    }
 );
 
-console.log(window.client);
+console.log(
+    "Wildstyle Supabase client loaded"
+);
