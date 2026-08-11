08ab3f88d85571468a87b7bf978cd8312d54b3b5
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2026-08-11 18:33:21 +0100

    Create auth-backup.js

---
+++ Chat/js/auth-backup.js
@@ -0,0 +1,53 @@
+// ==========================================
+// CHECK LOGIN
+// ==========================================
+
+(async () => {
+
+    const {
+        data: { session }
+    } = await client.auth.getSession();
+
+    if (!session) {
+
+    alert("APP TEST: NO SUPABASE SESSION");
+
+    return;
+
+}
+
+})();
+
+
+// ==========================================
+// LOGOUT
+// ==========================================
+
+async function logout() {
+
+    const {
+        data: { user }
+    } = await client.auth.getUser();
+
+    if (user) {
+
+        await client
+            .from("online_users")
+            .delete()
+            .eq("user_id", user.id);
+
+        await client
+            .from("chat_events")
+            .insert([{
+                message: "👋 " + user.email + " left the chat"
+            }]);
+
+    }
+
+    await client.auth.signOut();
+
+    window.location.href = "index.html";
+
+}
+
+window.logout = logout;
